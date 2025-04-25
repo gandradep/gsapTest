13c5c7fb07b45262325b01cd27eb5fa9f3c8217f
gandradep <eventsonido@gmail.com>
2025-04-25 17:48:00 +0800

    add quotes

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
 gsap.registerPlugin(SplitText);
 
 document.fonts.ready.then(() => {
-  let text = document.querySelector(".title");
+  const text = document.querySelector(".title");
   gsap.set(text, { opacity: 1 });
   let mySplitText = SplitText.create(text, {
     type: "chars, words",
     charsClass: "char"
   });
   let chars = mySplitText.chars;
+  let words = mySplitText.words;
+  gsap.fromTo(words,
+    {
 
+      x: 205,
+    },
+    {
+      duration: 1.25,
+      x: 0,
+      ease: "power2.out",
+      stagger: 0.01
+    }
+  );
   gsap.fromTo(chars,
     {
       opacity: 0,
       scale: 0,
-      y: 100,
-      x:50,
-      rotationX: -90,
-      rotationZ: -45,
-      transformOrigin: "50% 50%"
+      y: 80,
+      rotationX: -180,
+      rotationZ: -20,
+      transformOrigin: "50% 100%"
     },
     {
-      duration: 1,
+      duration: 1.25,
       opacity: 1,
       scale: 1,
       y: 0,
-      x:0,
       rotationX: 0,
       rotationZ: 0,
-      ease: "power1.out",
+      ease: "power2.out",
       stagger: 0.01,
     }
   );
 });
-
-
